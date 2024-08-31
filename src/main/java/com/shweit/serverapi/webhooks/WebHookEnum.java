af6b0d72677e9145edbf4885c4ebfe39199bbddc
Dennis van den Brock <dennis.vandenbrock@easybill.de>
2024-08-31 21:41:44 +0200

    Added `world_unload` WebHook

--- src/main/java/com/shweit/serverapi/webhooks/WebHookEnum.java
+++ src/main/java/com/shweit/serverapi/webhooks/WebHookEnum.java
@@ -40,7 +40,8 @@ public enum WebHookEnum {
     WEATHER_CHANGE("weather_change", "Triggered when the weather changes"),
     THUNDER_CHANGE("thunder_change", "Triggered when the thunder changes"),
     WORLD_LOAD("world_load", "Triggered when a world is loaded"),
-    WORLD_SAVE("world_save", "Triggered when a world is saved"),;
+    WORLD_SAVE("world_save", "Triggered when a world is saved"),
+    WORLD_UNLOAD("world_unload", "Triggered when a world is unloaded");
 
     public final String label;
     public final String description;
